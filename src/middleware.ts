d053f134d8e4ad17860154671a5133aeadd5ea9d
MrErenK <akbaseren4751@gmail.com>
2024-08-19 18:27:36 +0000

    refactor: Update middleware matcher to exclude icons folder

--- src/middleware.ts
+++ src/middleware.ts
@@ -31,5 +31,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|icons).*)"],
 };
